cebf73a7a54f0cd9ecd5bdc0d8a1c88e9a4ac572
kordax <dmorozov@beget.ru>
2014-07-04 13:12:57 +0400

    mysql client

--- job/mysql/main.cpp
+++ job/mysql/main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <my_global.h>
 #include <mysql.h>
-#include "string"
+#include <string.h>
 
 using namespace std;
 
-void main_loop(MYSQL* connection, string &input)
+void loop_start(MYSQL* connection, string &input)
 {
+
+	mysql_options(connection, MYSQL_SET_CHARSET_NAME, "CP1251");
+
 	const char* cinput;
 	while(true)
 	{
-		if (cin >> input && input == "quit" || input == "exit" || input == "leave");
+		if (cin >> input && input == "quit" || input == "exit" || input == "leave") return;
 		cinput = input.c_str();
 		if (mysql_query(connection, cinput) != 0)
 		{
-			cout << "ERROR" << endl;
+			system("\E[40;31m");
+			string error_output;
+			error_output = cinput;
+			cout << mysql_error(connection);
+			cout << " DEBUG INFO: #" << mysql_real_query(connection, cinput, strlen(cinput));
+			cout << "PARSED STRING: " << error_output << '\n';
+			system("\E[40;33m");
 		}
 		input.clear();
 	}
@@ -47,7 +56,11 @@ pbase = base.c_str();
 ppass = pass.c_str();
 
 if(!mysql_real_connect(&conn, phost, puser, ppass, pbase, 0, NULL, 0))
-	cout << "Error: can't connect to MySQL server\n";
+	{
+		cout << "Error: can't connect to MySQL server\n";
+		system("echo '\033[0m'");
+		return 1;
+	}
 else
 	cout << "Successfully connected!";
 
@@ -60,8 +73,7 @@ cout << endl;
 //system("echo '\033[0m'");
 
 string inp;
-const char* cinp;
 
-void main_loop();
-
+loop_start(marker, inp);
+system("echo '\033[0m'");
 }
