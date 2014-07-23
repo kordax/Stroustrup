b812f5e0b7cea0e4048389b17333a777d708fc04
kordax <dremor8686@mail.ru>
2014-07-23 19:43:51 +0400

    File_handle

--- exercises/19_12_1/main.cpp
+++ exercises/19_12_1/main.cpp
@@ -7,14 +7,20 @@ class File_handle
 {
 public:
     File_handle();
-    File_handle(const string& str);
+    File_handle(const string&);
     ~File_handle();
+
+private:
+    fstream* file_stream;
 };
+
+File_handle::File_handle(const string& _filename)
+{
+
+}
 
 int main()
 {
-
-    cout << "Hello World!" << endl;
     return 0;
 }
 
