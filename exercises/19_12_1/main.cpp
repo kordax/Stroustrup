ecc472a1e751decfa7f28e4bef8e5818a67d87c1
kordax <dremor8686@mail.ru>
2014-07-23 17:53:48 +0400

    file_handle

---
+++ exercises/19_12_1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class File_handle
+{
+public:
+    File_handle();
+    File_handle(const string& str);
+    ~File_handle();
+};
+
+int main()
+{
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
