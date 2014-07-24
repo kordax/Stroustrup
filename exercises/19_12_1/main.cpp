3efe515abe49b55c0dbef8cabdfceaacfe47b3e9
kordax <dremor8686@mail.ru>
2014-07-24 15:59:16 +0400

    File_handle

--- exercises/19_12_1/main.cpp
+++ exercises/19_12_1/main.cpp
@@ -16,11 +16,18 @@ private:
 
 File_handle::File_handle(const string& _filename)
 {
+    new (file_stream) fstream();
+    file_stream->open(_filename.c_str(), ios_base::in | ios_base::out);
+}
 
+File_handle::~File_handle()
+{
+    delete file_stream;
 }
 
 int main()
 {
+    File_handle("test.txt");
     return 0;
 }
 
