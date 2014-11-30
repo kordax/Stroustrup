61db647780d2ed721849ebff8031ee7c8e5bc837
kordax <dremor8686@mail.ru>
2014-12-01 02:24:33 +0300

    shared_memory

--- temp/shared_memory/allocator.h
+++ temp/shared_memory/allocator.h
@@ -13,33 +13,27 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-using namespace std;
-
 namespace kordax {
 
 template <class A>
 struct frame_s {
     int fd;
-    string memn;
+    std::string memn;
     A *addr;
     int quantity;
 };
 
 template <class A>
 class shmem_ruler {
-private:
-    void *pointer;
 public:
-    //void* to_memory(const string);
-    frame_s<A> to_memory(int n, const string);
-    void* construct(const int);
+    frame_s<A> to_memory(int n, const std::string);
 
-    shmem_ruler();
-    ~shmem_ruler();
+    shmem_ruler() {};
+    ~shmem_ruler() {};
 };
 
 template <class A>
-frame_s<A> to_memory(int n, const string memname)
+frame_s<A> shmem_ruler<A>::to_memory(int n, const std::string memname)
 {
     frame_s<A> frame;
     char *region_name = new char[memname.length() + 1];
@@ -51,7 +45,7 @@ frame_s<A> to_memory(int n, const string memname)
 
     void *ptr = mmap(0, frame_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); // Отображаем область в память для данного процесса
 
-    frame.addr = &ptr;
+    frame.addr = (A*)ptr;
     frame.fd = fd;
     frame.memn = memname;
     frame.quantity = n;
@@ -59,5 +53,15 @@ frame_s<A> to_memory(int n, const string memname)
 
     return frame;
 }
+/*
+template <class A>
+shmem_ruler<A>::shmem_ruler()
+{
+}
 
+template <class A>
+shmem_ruler<A>::~shmem_ruler()
+{
+}
+*/
 }
--- temp/shared_memory/main.cpp
+++ temp/shared_memory/main.cpp
@@ -8,13 +8,13 @@ int main()
     int *ptr = myframe.addr;
     *ptr = 1;
 
-    cout << "I'm parent process! My int is " << *ptr << std::endl;
+    std::cout << "I'm parent process! My int is " << *ptr << std::endl;
 
    pid_t pid = fork();
 
     if (pid == 0) {
         *ptr = *ptr++;
-        std::cout << "I'm child process! My int is " << std::endl;
+        std::cout << "I'm child process! My int is " << *ptr << std::endl;
         return(0);
     }
     else {
